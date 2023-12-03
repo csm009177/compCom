0e9d71ff1e5f34b4cb980760d55038fe5266a5f2
csm <csm009177@gmail.com>
2023-12-03 17:45:34 +0900

    server maked

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import http from 'http'
-const port = 3212
-const serv = http.createServer((res, req) => {
-  
-})
+import http from 'http';
+const port = 3212;
 
-serv.listen(port, ()=> {
+const serv = http.createServer((req, res) => {
+  res.end('hello');
+});
+
+serv.listen(port, () => {
   console.log(`http://localhost:${port}`);
-})
+});
