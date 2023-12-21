ca361016350cd8e48fa24dc448d255777eba0fc5
csm <csm009177@gmail.com>
2023-12-21 11:15:16 +0900

    practice obj, class

---
+++ syntax/prac.js
@@ -0,0 +1,15 @@
+// 객체로 만들기
+const choi = {
+  name : "choi-sungmin",
+  age : 45,
+  hometown : "youngchun"
+}
+
+// class로 만들기
+class presonClass {
+  choi() {
+    name : "choi-sungmin";
+    age : 45;
+    hometown : "youngchun";
+  }
+}
